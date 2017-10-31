e3dd415d63aea51bd2c151176da2a47700113ff8
boyyob747 <boysavana@gmail.com>
2017-10-31 10:20:31 +0700

    add file bootstrap,jquery add main.balde.php

---
+++ resources/views/sinhvien/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+@section('title','Sinh vien')
+@section('content')
+    <h1>{{$title or 'Title'}}</h1>
+    <p>{{$subtitle or 'Sub title'}}</p1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('sinhvien.index');
 });
 Route::get('group', function () {
     return "anothay";
