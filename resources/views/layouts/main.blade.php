4d9c1f008812be10ecb0fb5c9e17d4bf25c246ec
boyyob747 <boysavana@gmail.com>
2017-10-31 10:35:10 +0700

    add html.class

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,10 +1,14 @@
 <html>
     <head>
         <title>App Name - @yield('title')</title>
+        {!! Html::style('bootstrap/css/bootstrap.min.css') !!}
     </head>
     <body>
+      <button class="btn btn-primary">boy</button>
         <div class="container">
             @yield('content')
         </div>
+        {!! Html::script('js/jquery.min.js') !!}
+          {!! Html::script('bootstrap/js/bootstrap.min.js') !!}
     </body>
 </html>
